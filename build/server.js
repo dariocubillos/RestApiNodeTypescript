67bf54d30e7741f95183214ba766105f56a825f8
Alejandro Roa <alejodev04@gmail.com>
2020-08-14 16:48:31 -0400

    init

---
+++ build/server.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
+class Server {
+    constructor() {
+        this.app = express_1.default();
+        this.config();
+        this.routes();
+    }
+    config() {
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.use(morgan_1.default('dev'));
+        this.app.use(helmet_1.default());
+    }
+    routes() {
+        this.app.get('/', (req, res) => res.send('hello'));
+    }
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('server on port', this.app.get('port'));
+        });
+    }
+}
+const server = new Server();
+server.start();
+//npm run ts
+//npm run dev
