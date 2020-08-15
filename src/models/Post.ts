de5025dca5daf0b45bdbf4c2d39cc63e0a08e0dc
Alejandro Roa <alejodev04@gmail.com>
2020-08-15 10:06:08 -0400

    fix model

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -4,7 +4,7 @@ const PostSchema = new Schema({
     title: { type: String, required: true },
     slug: { type: String, required: true, unique: true, lowercase: true },
     content: { type: String, required: true },
-    imgUrl: { type: String, required: true },
+    imageUrl: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date }
 })
