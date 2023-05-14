e9d29e242123415cd0ed0c45fa0488629847b6c8
Dario Cubillos <dariocubillos1@gmail.com>
2023-05-14 01:52:52 -0600

    update try to fix

--- src/helper/mercadoLibreHelper.ts
+++ src/helper/mercadoLibreHelper.ts
@@ -1,20 +1,22 @@
 import axios, { AxiosResponse } from "axios";
 import { environment } from "../environments/environment";
 import { ProductMercadoLibre } from "../models/product-mercado-libre";
+const https = require('https');
 
 export class mercadoLibreHelper {
 
     constructor(){}
     
     async getProducts(searchQuery:string):Promise<AxiosResponse<any, ProductMercadoLibre>> {
-        const https = require('https');
-
-        return axios.get<any>(`${environment.mercadoLibreAPI}/sites/${environment.mercadoLibreSite}/search?q=${searchQuery}`, { headers: {
+        const instance = axios.create({
+            httpsAgent: new https.Agent({  
+              rejectUnauthorized: false
+            })
+          });
+        return instance.get<any>(`${environment.mercadoLibreAPI}/sites/${environment.mercadoLibreSite}/search?q=${searchQuery}`, { headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${environment.access_token}`
-        },
-        responseType: 'json',
-        httpsAgent: new https.Agent({ rejectUnauthorized: false })});
+            'Authorization': `Bearer ${environment.access_token}`,
+        }});
     }
 
 }
