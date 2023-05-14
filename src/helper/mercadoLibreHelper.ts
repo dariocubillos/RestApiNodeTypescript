eb6dbce3edb9fa79bb7b42d631b64f7ab74a95f9
Dario Cubillos <dariocubillos1@gmail.com>
2023-05-14 02:11:56 -0600

    Revert "update to try fix error"
    This reverts commit c96b060da516c365b9c6813cdaf393b1667dfb8f.

--- src/helper/mercadoLibreHelper.ts
+++ src/helper/mercadoLibreHelper.ts
@@ -7,14 +7,10 @@ export class mercadoLibreHelper {
     constructor(){}
     
     async getProducts(searchQuery:string):Promise<AxiosResponse<any, ProductMercadoLibre>> {
-        const https = require('https');
-
         return axios.get<any>(`${environment.mercadoLibreAPI}/sites/${environment.mercadoLibreSite}/search?q=${searchQuery}`, { headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${environment.access_token}`
-        },
-        responseType: 'json',
-        httpsAgent: new https.Agent({ rejectUnauthorized: false })});
+        }});
     }
 
 }
